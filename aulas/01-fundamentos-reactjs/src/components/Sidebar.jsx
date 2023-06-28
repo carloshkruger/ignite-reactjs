ccda9907bc329029e347b828f176045068f140ec
carloshkruger <carloshkruger@gmail.com>
2023-06-28 09:26:34 -0300

    project 01: avatar component

--- aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react"
+import { Avatar } from "./Avatar"
 
 import styles from "./Sidebar.module.css"
 
@@ -8,7 +9,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=300&q=40" alt="" />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/18452687?v=4" alt="" />
+        <Avatar src="https://avatars.githubusercontent.com/u/18452687?v=4" />
         <strong>Diego Fernandes</strong>
         <span>Web Developer</span>
       </div>
